3c1f1a05f7351240c1719b27b292b914c69f8fd1
Gabriel Malinosqui <gabriel@kodus.io>
2026-07-11 15:20:42 -0300

    Add second raw-cents violation to trigger review

--- services/billing/invoice.rb
+++ services/billing/invoice.rb
@@ -5,3 +5,11 @@ module Billing
     end
   end
 end
+
+module Billing
+  class Discount
+    def apply(amount_cents, percent)
+      amount_cents - (amount_cents * percent / 100.0)
+    end
+  end
+end
